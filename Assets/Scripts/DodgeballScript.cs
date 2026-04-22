a9dcfc8c8ebdd3f79da4a4ca46177ba50d91c69c
jyuicy <julianross651@gmail.com>
2026-04-21 17:27:39 -0700

    dodgeball vertBounce
    ball now bounces vertically. still need to make horizontal bouncing, but this somehow took 2 hours and iwanna kms rn.

--- Assets/Scripts/DodgeballScript.cs
+++ Assets/Scripts/DodgeballScript.cs
@@ -1,19 +1,63 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class DodgeballScript : MonoBehaviour
 {
     
     public bool _isLive = false;
     public string originPlayer = "";
+    public float bounce = .07f;
 
- 
+
+    private Rigidbody2D rb;
+    private CircleCollider2D circle;
+    private float radius;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        circle = GetComponent<CircleCollider2D>();
+
+        radius = circle.radius * Mathf.Max(transform.localScale.x, transform.localScale.y);
+    }
+
     //Reset to "not live" and remove any "origin player" when im thrown
     void OnCollisionEnter2D(Collision2D col)
     {
-       if(col.gameObject.CompareTag("Ground"))
+        Vector2 rbVelo = col.relativeVelocity;
+        Debug.Log("relative velo= " + rbVelo);
+
+        if (col.gameObject.CompareTag("Terrain"))
         {
-            _isLive = false;
-            originPlayer = "";
+            Vector2 hitTerrain = col.contacts[0].normal;
+
+            //rb.linearVelocity = reflectDir * bounce;
+
+            if (hitTerrain.y > .5f)
+            {
+                _isLive = false;
+                originPlayer = "";
+                StartCoroutine(vertBounce(rbVelo));
+            }
+            if (hitTerrain.y < .5f)
+            {
+                StartCoroutine(vertBounce(rbVelo));
+            }
         }
+    }
+
+    //private IEnumerator horiBounce()
+    //{
+    //    horiSpeed *= -.5f;
+    //    yield return new WaitForSeconds(1);
+    //}
+
+    private IEnumerator vertBounce(Vector2 bounceVelo)
+    {
+        Debug.Log("start vert bounce");
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, bounceVelo.y * bounce);
+        yield return new WaitForSeconds(0.1f);
     }
 }
